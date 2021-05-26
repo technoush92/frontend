5c1e2b71996e5cfbd6cd93ffd05f4d146556268c
Tasadduq Ali <62251149+tasduq@users.noreply.github.com>
2021-05-27 04:19:04 +0500

    fixed card design

--- src/Components/Searchcard.js
+++ src/Components/Searchcard.js
@@ -24,11 +24,11 @@ const Searchcard = ({
     <div className="border  m-2" style={{ borderRadius: "12px" }}>
       <div className="row ">
         <div className="col-5 px-0">
-          <div>
+          <div className="" style={{}}>
             <img
               src={image}
               className="  img-fluid my-2 ml-3"
-              style={{ height: "90%", width: "90%" }}
+              style={{ height: "90%", width: "90%", borderRadius: "12px" }}
             />
           </div>
         </div>
@@ -47,16 +47,24 @@ const Searchcard = ({
               },
             }}
           >
-            <div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "90%",
+              }}
+            >
               <div className="  w-100 ">
                 {title && (
-                  <h5>
+                  <h5 className="mt-2">
                     {" "}
                     {title.length > 10
                       ? (title = `${title.slice(0, 18)}...`)
                       : title}
                   </h5>
                 )}
+                <h5 className="m-0 p-0">Price : {price}</h5>
               </div>
               <div className="text-muted">
                 <p className="m-0 p-0">
@@ -66,8 +74,8 @@ const Searchcard = ({
                     ? (description = `${description.slice(0, 75)}...`)
                     : description}
                 </p>
-                <p className="m-0 p-0">Price : {price}</p>
-                {/* <p className="m-0 p-0">Date : {date}</p> */}
+
+                <p className="m-0 p-0">Date : {date}</p>
               </div>
             </div>
           </Link>
