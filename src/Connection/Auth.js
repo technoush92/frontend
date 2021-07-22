8470ffced2d1df02d41c39ad21f0bcdad61de007
Tasadduq Ali <62251149+tasduq@users.noreply.github.com>
2021-07-22 16:01:25 +0500

    revision 6

--- src/Connection/Auth.js
+++ src/Connection/Auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-let url = "http://localhost:3001";
-// let url = "https://adsbackendapp.herokuapp.com";
+// let url = "http://localhost:3001";
+let url = "https://adsbackendapp.herokuapp.com";
 
 const signupUser = async (data) => {
   console.log(data);
