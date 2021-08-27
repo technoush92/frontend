032fe8a94b873c094d1899add280d5831ddfd39f
Tasadduq Ali <62251149+tasduq@users.noreply.github.com>
2021-08-27 15:32:28 +0500

    fixing

--- src/Connection/Messages.js
+++ src/Connection/Messages.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // let url = "http://localhost:3001";
-let url = "https://adsbackendapi.herokuapp.com";
+let url = "https://adsbackend2.herokuapp.com/";
 
 const sendMessage = async (data) => {
   console.log(data);
